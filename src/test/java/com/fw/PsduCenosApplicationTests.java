2662a2248f244e23e211dba13326957250b1a388
yqf <940695836@qq.com>
2020-06-29 15:24:47 +0800

    2020-6-29

--- src/test/java/com/fw/PsduCenosApplicationTests.java
+++ src/test/java/com/fw/PsduCenosApplicationTests.java
@@ -67,7 +67,7 @@ class PsduCenosApplicationTests {
         grading.setId(14);
 
         Rule rule = new Rule();
-        rule.setRuleContent("hi siri ");
+        rule.setRuleContent("hi ");
         rule.setGradingId(grading.getId());
 
         List<Rule> rules = new ArrayList<>();
