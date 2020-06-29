a0033c8a00200c72b7add4789f717a791660e426
yqf <940695836@qq.com>
2020-06-29 14:37:14 +0800

    2020-6-29

--- src/test/java/com/fw/PsduCenosApplicationTests.java
+++ src/test/java/com/fw/PsduCenosApplicationTests.java
@@ -67,7 +67,7 @@ class PsduCenosApplicationTests {
         grading.setId(14);
 
         Rule rule = new Rule();
-        rule.setRuleContent("五官端正(0)");
+        rule.setRuleContent("hello world");
         rule.setGradingId(grading.getId());
 
         List<Rule> rules = new ArrayList<>();
