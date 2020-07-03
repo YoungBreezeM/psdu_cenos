09b22aa02f3613f477f03bc681204f094b4bcd85
yqf <940695836@qq.com>
2020-07-03 20:58:54 +0800

    2020-7-2

--- src/main/java/com/fw/config/MailConfig.java
+++ src/main/java/com/fw/config/MailConfig.java
@@ -11,6 +11,7 @@ import org.thymeleaf.TemplateEngine;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.util.Properties;
 
 /**
  * @author yqf
@@ -25,10 +26,26 @@ public class MailConfig {
     JavaMailSenderImpl mailSender() throws MessagingException {
         SysMail sysMail = sysMailMapper.findOne();
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
+
+        javaMailSender.setDefaultEncoding("UTF-8");
+
+        Properties properties = new Properties();
+        //properties.setProperty("mail.debug", "true");//启用调试
+        //properties.setProperty("mail.smtp.timeout", "1000");//设置链接超时
+        //设置通过ssl协议使用465端口发送、使用默认端口（25）时下面三行不需要
+        //开启认证
+        properties.setProperty("mail.smtp.auth", "true");
+        //设置ssl端口
+        properties.setProperty("mail.smtp.socketFactory.port", "465");
+        properties.setProperty("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+
+
         javaMailSender.setHost(sysMail.getHost());
         javaMailSender.setProtocol(sysMail.getProtocol());
         javaMailSender.setUsername(sysMail.getUserName());
         javaMailSender.setPassword(sysMail.getPassword());
+
+        javaMailSender.setJavaMailProperties(properties);
 
         return javaMailSender;
     }
